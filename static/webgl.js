d577b36966d5aa50d9cd801a07dbcd4f9615ff59
Lawrence Neal <lawrencewneal@gmail.com>
2013-11-24 15:46:33 -0800

    Generate textures on-the-fly from words
    Based on Harry Jones' excellent demo code:
    http://delphic.me.uk/webgltext.html

--- static/webgl.js
+++ static/webgl.js
@@ -83,6 +83,18 @@ function handleLoadedTexture(texture) {
   gl.bindTexture(gl.TEXTURE_2D, null);
 }
 
+function handleLoadedTexture(texture, textureCanvas) {
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureCanvas); // This is the important line!
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
 function initTestTexture() {
   var tex;
   tex = gl.createTexture();
@@ -152,7 +164,7 @@ function initGhostBuffer() {
 // Clockwise gets culled
 function initCupBuffer() {
   var pi = 3.1415;
-  var res = 100;
+  var res = 500;
   var verts = [];
   var norms = [];
   var tex = [];
